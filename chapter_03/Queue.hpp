e5ad620a5af54012d3cb5344701e224d29381280
PiscesDante <libingjian2580@163.com>
2018-05-02 17:15:27 +0800

    add some content of queue

--- chapter_03/Queue.hpp
+++ chapter_03/Queue.hpp
@@ -6,8 +6,8 @@
 template <typename T>
 struct QueueNode {
     T val;
-    QueueNode<T>* next;
-    QueueNode<T>(T d, QueueNode<T>* n = nullptr) :
+    QueueNode* next;
+    QueueNode(T d, QueueNode* n = nullptr) :
         val(d), next(n) { }
 };
 
@@ -15,20 +15,19 @@ struct QueueNode {
 // ==============================================================
 template <typename T>
 class Queue {
-
 public:
-    Queue() = default; // 默认构造函数
-    Queue& Queue(const Queue& rhs); // 拷贝构造函数
-    Queue operator = (const Queue& rhs); // 拷贝赋值运算符
+    Queue() = default;
+    Queue(const Queue& rhs);
+    Queue& operator = (const Queue& rhs);
 
-    bool empty() const { return queue_size == 0; } // DONE
-    int size() const { return queue_size; } // DONE
-    T front();
-    T back();
+    bool empty() const { return queue_size == 0; }
+    int size() const { return queue_size; }
+    T& front();
+    T& back();
     void push(T input_val);
     void pop();
 
-    ~Queue(); // 析构函数
+    ~Queue();
 
 private:
     QueueNode<T>* head = nullptr;
@@ -37,19 +36,68 @@ private:
 
 };
 
+// CLass Queue Definition
+// ==============================================================
+
+// 拷贝构造函数定义
 template <typename T>
-Queue& Queue<T>::Queue(const Queue& rhs) {
-    if (this != &rhs && rhs->head != nullptr) {
-        QueueNode<T>* rhs_sentinel = rhs.head;
-        while (rhs_sentinel != nullptr) {
-            if (head == nullptr) {
-                head = new QueueNode(rhs_sentinel->val);
-                tail = head;
-            } else {
-                // TODO
-            }
-        }
+Queue<T>::Queue(const Queue& rhs) {
+    QueueNode<T>* temp = rhs.head;
+    while (temp != nullptr) {
+        push(temp->val);
+        temp = temp->next;
     }
+}
+
+// 拷贝赋值运算符
+template <typename T>
+Queue<T>& Queue<T>::operator = (const Queue& rhs) {
+    if (this != &rhs) {
+        ~Queue();
+        Queue(rhs);
+    }
+    return *this;
+}
+
+// 获取队头（可修改的引用）
+template <typename T>
+T& Queue<T>::front() { return *head; }
+
+// 获取队尾（可修改的引用）
+template <typename T>
+T& Queue<T>::back() { return *tail; }
+
+// 入队
+template <typename T>
+void Queue<T>::push(T input_val) {
+    QueueNode<T>* new_node = new QueueNode<T>(input_val);
+    if (head == nullptr) {
+        head = new_node;
+        tail = head;
+    } else {
+        tail->next = new_node;
+        tail = new_node;
+    }
+    ++queue_size;
+}
+
+// 出队
+template <typename T>
+void Queue<T>::pop() {
+    if (empty()) return;
+    QueueNode<T>* del = head;
+    head = head->next;
+    delete del;
+    del = nullptr;
+    --queue_size;
+}
+
+// 析构函数
+template <typename T>
+Queue<T>::~Queue() {
+    while (!empty()) pop();
+    head = nullptr;
+    tail = nullptr;
 }
 
 #endif
